440ab91a259ace679e8fded896fe8b508350ae1e
Susan Meyers <smmeyers24@gmail.com>
2018-10-05 18:36:08 +0000

    Done.

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,3 +1,12 @@
 class Fish
-  # code goes here
+  
+  attr_accessor :fish, :mood
+  attr_reader :name
+  
+  def initialize(name)
+    @fish = fish
+    @name = name
+    @mood = "nervous"
+  end
+    
 end
