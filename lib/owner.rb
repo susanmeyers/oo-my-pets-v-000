f5b44e0fbdb072a386a6575c43285164bcc5d2a4
Susan Meyers <smmeyers24@gmail.com>
2018-10-06 01:30:44 +0000

    Done.

--- lib/owner.rb
+++ lib/owner.rb
@@ -5,19 +5,23 @@ class Owner
   attr_accessor :pets, :owner
   
   def initialize(species)
-   
     @pets = {fishes: [], cats: [], dogs: []}
     @@all << self
+    @owner = owner
   end
     
   def self.all
     @@all
   end
   
-  def self.reset_all
-    owner.new.count
-    
+  def self.count
+   @@all.length
   end
+  
+  def self.reset_all
+    @@all.clear
+  end
+   
 end
   
   
